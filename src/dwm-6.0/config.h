332fedca14636cb0e63670c654d1cba9d312fabd
Hubi <constantine17@gmail.com>
2013-05-29 15:10:35 +0200

    included xft patch changed font to inconsolata

--- src/dwm-6.0/config.h
+++ src/dwm-6.0/config.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";
+/*static const char font[]            = "-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*";*/
+static const char font[]            = "Inconsolata:medium:size=10";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
@@ -58,20 +59,23 @@ static const char *ncmpcpp[]    = { "urxvt", "-e", "ncmpcpp", NULL };
 static const char *browser[]    = { "firefox", NULL };
 static const char *mail[]       = { "thunderbird", NULL };
 static const char *chat[]       = { "pidgin", NULL } ;
+static const char *layoutsw[]   = { "/home/hubi/bin/layoutswitch.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = layoutsw } },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -123,5 +127,7 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkTagBar,            0,              Button4,        shiftview,      {.i = +1 } },
 	{ ClkTagBar,            0,              Button5,        shiftview,      {.i = -1 } },
+	{ ClkRootWin,           0,              Button4,        shiftview,      {.i = +1 } },
+	{ ClkRootWin,           0,              Button5,        shiftview,      {.i = -1 } },
 };
 
